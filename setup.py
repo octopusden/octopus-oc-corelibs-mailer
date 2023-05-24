04ed8229fb421156cf6e5de3015e7e79e331672d
Aleksey Knyazev <ctj_yebbs@inbox.ru>
2023-05-24 13:50:50 +0300

    setup.py: PEP8-formatting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 import glob
 
-__version="1.0.1"
+__version = "1.0.1"
 
 def list_recursive(app, directory, extension="*"):
     dir_to_walk = os.path.join(app, directory)
